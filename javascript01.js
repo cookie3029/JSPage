40dc437ec793281e1905295fdf9aedde192e49e2
cookie3029 <whitewalls@naver.com>
2024-04-04 17:43:06 +0900

    feat: create javascript01.js

---
+++ javascript01.js
@@ -0,0 +1,27 @@
+// 자바스크립트 변수, 자료형, 함수, 조건문, 반복문
+
+// 변수
+let a = 'a';
+const b = 'b';
+
+let camelCase; 
+let snake_case; 
+let PascalCase; 
+let $case1; 
+let _case2; 
+
+let bread = '빵';
+let creamBread = '크림빵';
+bread = creamBread;
+console.log(bread);
+console.log('첫번째 빵은 : ', bread);
+console.log(`빵은 : ${bread}`);
+
+// 자료형
+
+
+// 함수
+
+// 조건문
+
+// 반복문
