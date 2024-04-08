4bb1387cedc8da763328d9edb2ee463af3c8fe5d
cookie3029 <whitewalls@naver.com>
2024-04-08 09:56:03 +0900

    create 1 file and update 1 file

--- javascript01.js
+++ javascript01.js
@@ -1,27 +1,36 @@
 // 자바스크립트 변수, 자료형, 함수, 조건문, 반복문
 
-// 변수
-let a = 'a';
-const b = 'b';
+// ------------------------------ 복습 ------------------------------
 
-let camelCase; 
-let snake_case; 
-let PascalCase; 
-let $case1; 
-let _case2; 
+// [1] 변수
+// - 변수 종류 : var, let, const
+// - 변수 명명규칙 : 이름, 규칙, 표기법
+// - 연산자(Operator) 종류 : 할당, 비교, 산술, 비트, 논리, 문자열, 조건, 쉼표, 단항, 관계, 옵셔널체이닝, 널 병합
+// - 값 할당, 변수 호출
+// - 메모리 관리, 가비지 컬렉션, 메모리 생존주기(할당, 사용(읽기, 쓰기), 해제)
 
-let bread = '빵';
-let creamBread = '크림빵';
-bread = creamBread;
-console.log(bread);
-console.log('첫번째 빵은 : ', bread);
-console.log(`빵은 : ${bread}`);
+// [2] 자료형
+// - 자료형 종류 : Number, BitInt, String, Boolean, Symbol, Undefined, Null
+// - 복합 타입 : Object, Function, Array
+// - 문자열(String) : 큰 따옴표, 작은 따옴표, 백틱
+// - 타입 체크(typeof) : 타입 확인, 타입 변환
+// - 객체(object) : 일반객체, 배열, 함수, 날짜, 정규표현식, 에러, 내장 객체, 전역 객체
+// - 객체 메서드 : Object.entries(), Object.keys(), Object.freeze(), Object.seal(), Object.is(), Object.assign()
+// - 배열 구조분해할당 : 요소 할당, ...rest 연산자
+// - 배열(array) : 배열 초기화(빈배열), 요소 추가, 확장, 삭제, 길이 늘리고 줄임
+// - 배열 메서드 : concat(), filter(), map(), slice(), sort(), splice(), forEach(), find(), join(),includes(), reduce(), push(), pop(), 그 외 array 메서드 등등
 
-// 자료형
+// [3] 함수
+// - 함수 종류 : 함수 선억식, 함수 표현식, 화살표 함수,
+// - 지역변수, 전역변수, 메서드, 매개변수(parameter), 전달인자(argument)
+// - 생성자 함수 : new 키워드, new Function(), 메서드(call, apply, bind)
+// - 재귀함수
+// - Class 함수 : 생성자(constructor), 속성(properties), 메서드(methods), 상속(inheritance), 캡슐화(encapsulation)
 
+// [4] 반복문
+// - 반복문 종류 : for, for in, for of, while, do while
+// - 반복문과 같이 사용되는 키워드 : break, continue, switch
+// - 배열과 객체로 반복문 활용
 
-// 함수
-
-// 조건문
-
-// 반복문
+// [5] 조건문
+// - 조건문 구분 : if, else, else if, switch
